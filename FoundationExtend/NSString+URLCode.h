896ebf489bc5a0ed210b544063dcbe4f66cc2c71
silence <jquan.zhu@gmail.com>
2013-11-04 09:47:15 +0800

    modify urlcode

---
+++ FoundationExtend/NSString+URLCode.h
@@ -0,0 +1,13 @@
+//
+//  NSString+URLCode.h
+//  FoundationExtend
+//
+//  Created by ZhuJiaQuan on 13-11-4.
+//  Copyright (c) 2013年 5codelab. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (URLCode)
+
+@end
---
+++ FoundationExtend/NSString+URLCode.m
@@ -0,0 +1,13 @@
+//
+//  NSString+URLCode.m
+//  FoundationExtend
+//
+//  Created by ZhuJiaQuan on 13-11-4.
+//  Copyright (c) 2013年 5codelab. All rights reserved.
+//
+
+#import "NSString+URLCode.h"
+
+@implementation NSString (URLCode)
+
+@end
