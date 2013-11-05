8e4605ddf83268edcf09c28642d7869f64843f6f
silence <jquan.zhu@gmail.com>
2013-11-05 11:02:09 +0800

    ci

--- FoundationExtend/FoundationExtend.h
+++ FoundationExtend/FoundationExtend.h
@@ -6,18 +6,6 @@
 //  Copyright (c) 2013年 5codelab. All rights reserved.
 //
 
-//#import <FoundationExtend/ARCMacros.h>
-//#import <FoundationExtend/GCDDispatch.h>
-//#import <FoundationExtend/NSString+Utilities.h>
-//#import <FoundationExtend/NSNumber+Utilities.h>
-//#import <FoundationExtend/NSData+Base64.h>
-//#import <FoundationExtend/NSString+Base64.h>
-//#import <FoundationExtend/NSData+MD5.h>
-//#import <FoundationExtend/NSString+MD5.h>
-//#import <FoundationExtend/NSData+AES.h>
-//#import <FoundationExtend/NSString+AES.h>
-//#import <FoundationExtend/NSDate+Utilities.h>
-
 #import "FoundationExtend/ARCMacros.h"
 #import "FoundationExtend/GCDDispatch.h"
 #import "FoundationExtend/NSObject+Utilities.h"
